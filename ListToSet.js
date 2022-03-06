9fe4ee8bc6b66c4ef3fbeb61bd5b3892d0a10011
sergzh1703 <34885493+sergzh1703@users.noreply.github.com>
2022-03-06 20:10:11 +0300

    Update ListToSet.js
    add second variant

--- ListToSet.js
+++ ListToSet.js
@@ -6,3 +6,16 @@ console.log(findList.length)
 let uniqueList = [...new Set(findList)]
 console.log(uniqueList)
 console.log(uniqueList.length)
+
+//Variant 2
+
+const l = '[{"user5":5},{"user2":2}"user2","user2""user5"]';
+let patern = /user\d+/g;
+findList = l.match(patern)
+console.log(findList)
+console.log(findList.length)
+let uniqueList = findList.filter((c,index) => {
+ return findList.indexOf(c)===index;
+});
+console.log(uniqueList)
+console.log(uniqueList.length)
